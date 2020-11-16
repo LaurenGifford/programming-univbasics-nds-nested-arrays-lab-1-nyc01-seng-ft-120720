10fb1fadefabe5f01d86d03ac4f41d3ec719fca5
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-16 17:29:21 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,4 +1,3 @@
-# Examples inspired by U. S. National Organic Standards
 
 ORGANIC_PRODUCE = [
   "Strawberries",
@@ -35,9 +34,6 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
   matrix[row][column] = new_value
   matrix
 end
